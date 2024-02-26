9ce059f10232077cb5d1992fa948b138844cf170
“Jonathan <racknarow1@gmail.com>
2024-02-25 23:55:07 -0500

    add contact

--- src/app/contacto/page.js
+++ src/app/contacto/page.js
@@ -1,66 +1,55 @@
 import Breadcrumbs from '../components/breadcumbs/Breadcumbs';
+import '../scss/pages/contacto/contacto.scss';
+import { Icon } from 'react-icons-kit';
+import { ic_facebook } from 'react-icons-kit/md/ic_facebook';
+import { instagram } from 'react-icons-kit/fa/instagram';
+import { whatsapp } from 'react-icons-kit/fa/whatsapp';
+import { location } from 'react-icons-kit/icomoon/location';
+import { phone } from 'react-icons-kit/icomoon/phone';
+import {ic_alternate_email} from 'react-icons-kit/md/ic_alternate_email'
 
 const page = () => {
     return (
         <div className="contacto_main_container">
             <Breadcrumbs link="/contacto" nombre="Contacto" />
 
-            <div className="container mt-5">
-                <div className="row">
-                    <div className="col-md-6 offset-md-3">
-                        <h2 className="text-center mb-4">Puedes Ubicarnos con los siguientes Datos.</h2>
-                        <p className="text-center">¡Estamos aquí para ayudarte!</p>
-                        <ul className="list-unstyled text-center mb-4">
-                            <li>
-                                <i className=""></i>
-                                Dirección: Cra 26 # 6A – 26, Sogamoso – Boyaca
-                            </li>
-                            <li>
-                                <i className=""></i>
-                                ¡Servicio en Todo Boyacá!
-                            </li>
-                            <li>
-                                <i className=""></i>Teléfono:
-                                +57 323 364 2951
-                            </li>
-                            <li>
-                                <i className=""></i>Correo
-                                electrónico: biozonosco@gmail.com
-                            </li>
-                        </ul>
-                        <p className="text-center">
-                            Síguenos en las redes sociales:
-                        </p>
-                        <div className="text-center">
-                            <a
-                                href="#"
-                                className="btn btn-outline-primary mr-2"
-                            >
-                                
-                            </a>
-                            <a
-                                href="#"
-                                className="btn btn-outline-info mr-2"
-                            >
-                                <i className=""></i>
-                            </a>
-                            <a
-                                href="#"
-                                className="btn btn-outline-danger mr-2"
-                            >
-                                <i className=""></i>
-                            </a>
+            <div className="main_container">
+                <h2 className="title">
+                    Puedes ubicarnos con los siguientes Datos ya que...
+                </h2>
 
-                            
-                            <a
-                                href="#"
-                                className="btn btn-outline-success"
-                            >
-                                <i className=""></i>
-                            </a>
-                            
-                        </div>
-                    </div>
+                <p className="subtitle">¡Estamos aquí para ayudarte!</p>
+
+                <ul className="list-unstyled text-center mb-4">
+                    <li className="info">
+                        <Icon size={25} icon={phone} /> +57 323 364 2951
+                    </li>
+
+                    <li className="info">
+                    <Icon size={25} icon={ic_alternate_email} /> biozonosco@gmail.com
+                    </li>
+
+                    <li className="info">
+                        <Icon size={25} icon={location} /> Cra 26 # 6A – 26,
+                        Sogamoso – Boyaca
+                    </li>
+
+                    
+                    <li className="info all">¡Servicio en Todo Boyacá!</li>
+                </ul>
+                <p className="info plus">
+                    Contactanos también por nuestras redes sociales
+                </p>
+                <div className="icons_container">
+                    <a href="#" className="">
+                        <Icon size={50} icon={ic_facebook} />
+                    </a>
+                    <a href="#" className="">
+                        <Icon size={50} icon={instagram} />
+                    </a>
+                    <a href="#" className="">
+                        <Icon size={50} icon={whatsapp} />
+                    </a>
                 </div>
             </div>
         </div>
