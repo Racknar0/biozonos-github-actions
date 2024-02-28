5186718463e3f98f608fde86a7bdb742f6400ad5
“Jonathan <racknarow1@gmail.com>
2024-02-28 10:00:17 -0500

    change favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        
+      </head>
       <AOSImport />
       <body className={inter.className}>
         <NavBar />
