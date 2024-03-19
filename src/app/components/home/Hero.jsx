c4dd566d4196947de756b08df94bc96f6c1c3719
“Jonathan <racknarow1@gmail.com>
2024-03-19 14:33:58 -0500

    add improves

--- src/app/components/home/Hero.jsx
+++ src/app/components/home/Hero.jsx
@@ -1,12 +1,19 @@
-import Image from 'next/image'
-import '../../scss/pages/home/hero.scss'
+import Image from 'next/image';
+import '../../scss/pages/home/hero.scss';
 
 const Hero = () => {
-  return (
-    <div className='hero_container'>
-        <Image src="/home/water2.png" alt="ozonos_water_hero_image" objectPosition='right' width={5000} height={2200} />
-    </div>
-  )
-}
+    return (
+        <div className="hero_container">
+            <Image
+                src="/home/water.png"
+                alt="ozonos_water_hero_image"
+                objectPosition="right"
+                width={1920}
+                height={852}
+                loading='eager'
+            />
+        </div>
+    );
+};
 
-export default Hero
+export default Hero;
